e5e291efb529eb9cd9c73c02652a1b809aa7294d
Balakrishna-Tanneeru <jva.krishna@gmail.com>
2023-12-20 17:42:02 +0530

    Added comment at ln no 9

--- src/main/java/com/bk/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bk/demo/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+//Global exception class
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @Value(value = "${data.exception.message1}")
