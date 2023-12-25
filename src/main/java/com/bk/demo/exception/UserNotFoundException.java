ad03f314b87be7da0a29f1130c15b1320b00ee71
Balakrishna-Tanneeru <jva.krishna@gmail.com>
2023-12-25 18:33:48 +0530

    Added comment at ln 5

--- src/main/java/com/bk/demo/exception/UserNotFoundException.java
+++ src/main/java/com/bk/demo/exception/UserNotFoundException.java
@@ -2,7 +2,7 @@ package com.bk.demo.exception;
 
 public class UserNotFoundException extends RuntimeException {
     private String message;
-
+//Constructor for UserNotFoundException
     public UserNotFoundException(String message) {
         super(message);
         this.message = message;
